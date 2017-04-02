f515fdb9f9d44dc34a6788d1da851537401ea76b
h6gs <harrystirling94@gmail.com>
2017-04-02 13:28:23 +0100

    grid working + random color assignment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('hi')
+
+const blocks = [];
+
+function makeBlocks() {
+  // assign block values
+  const blockValues = ['red','yellow','green','blue','purple'];
+  return blockValues[Math.floor(Math.random() * blockValues.length)];
+}
+
+//loop to make game blocks
+for (let i = 0; i < 100; i++) {
+  let block = makeBlocks();
+  blocks.push(block);
+
+}
+
+//add blocks to page
+for (let i = 0; i < blocks.length; i++) {
+  // let div = document.createElement('div');
+  // div.innerHTML = blocks[i];
+  // document.body.appendChild(div);
+  // $('body').append('<div id="blocks' + '"class=blocky></div>');
+  $('<div>', { 'class': blocks[i] }).appendTo(document.body);
+  console.log(blocks[i]);
+}
+
+
+
+// for (let i = 0; i < blocks.length; i++) {
+//   // Create block div
+//   let div = $('<div></div>');
+//   div.innerHTML = blocks[i];
+//   $('.inner').append(div);
+// }
