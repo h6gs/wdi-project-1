54ffe52343aa9fbd75e0b1f265384e7bd5aa3ffe
h6gs <harrystirling94@gmail.com>
2017-04-03 11:35:39 +0100

    starting to make game/block logic

--- js/app.js
+++ js/app.js
@@ -16,3 +16,31 @@ for (let i = 0; i < 225; i++) {
 for (let i = 0; i < blocks.length; i++) {
   $('<div>', {id: [i], 'class': blocks[i]}).appendTo('main');
 }
+
+//change color of key square (top left)
+$('[name="redButton"]').on('click', function() {
+  $('#0').attr('class','red');
+});
+
+//change color of next cells until next red.
+$('[name="redButton"]').on('click', function() {
+  if ($('#0').next().attr('red')) {
+    $('#0').nextUntil('.red').attr('class','red');
+  } else {
+    console.log('no');
+  }
+
+});
+
+// $('#0').next(),console.log(this);
+
+
+
+
+
+
+// click color button
+// key block (top left) changes to that color
+// then evaluate if immediately adjacent blocks match the colour clicked
+// if yes, colour is added and the adjacent block(s) join to the winning array
+// repeat
